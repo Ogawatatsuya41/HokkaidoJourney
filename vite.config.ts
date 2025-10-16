fe00c819411e09a0604505b26f5e7b7b32a265f3
Ogawatatsuya41 <82919942+Ogawatatsuya41@users.noreply.github.com>
2025-10-16 11:43:11 +0900

    Update vite.config.ts2

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-          await import("@replit/vite-plugin-dev-banner").then((m) =>
-            m.devBanner(),
-          ),
-        ]
-      : []),
-  ],
+  plugins: [react()],
+  // Viteにindex.htmlがある場所を教える
+  root: path.resolve(__dirname, "client"),
   resolve: {
     alias: {
-      // この部分はVercelが理解できるようにパスの書き方を修正しました。
+      // @が指す場所を教える
       "@": path.resolve(__dirname, "client/src"),
-      "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets"),
     },
   },
-  // Vercelの設定と競合するため、root と build の設定は削除しました。
+  build: {
+    // 完成したファイルの出力先をプロジェクトのルート直下の`dist`フォルダに指定
+    outDir: path.resolve(__dirname, "dist"),
+  },
 });
