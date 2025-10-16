aa08cbf533103a957efe8833e0d0f9a06416fc0a
Ogawatatsuya41 <82919942+Ogawatatsuya41@users.noreply.github.com>
2025-10-16 12:59:56 +0900

    Update vite.config.ts3

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Viteにindex.htmlがある場所を教える
-  root: path.resolve(__dirname, "client"),
   resolve: {
     alias: {
-      // @が指す場所を教える
+      // 以前消してしまった @assets と @shared の設定を戻しました
       "@": path.resolve(__dirname, "client/src"),
+      "@assets": path.resolve(__dirname, "attached_assets"),
+      "@shared": path.resolve(__dirname, "shared"),
     },
   },
   build: {
+    // Viteにビルドの開始点を教えます
+    rollupOptions: {
+      input: path.resolve(__dirname, "client/index.html"),
+    },
     // 完成したファイルの出力先をプロジェクトのルート直下の`dist`フォルダに指定
     outDir: path.resolve(__dirname, "dist"),
   },
