eb07b65e82e06d7b8d6c591acb4df2955b623412
Ogawatatsuya41 <82919942+Ogawatatsuya41@users.noreply.github.com>
2025-10-16 13:08:03 +0900

    Update vite.config.ts4

--- vite.config.ts
+++ vite.config.ts
@@ -6,18 +6,16 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // 以前消してしまった @assets と @shared の設定を戻しました
       "@": path.resolve(__dirname, "client/src"),
       "@assets": path.resolve(__dirname, "attached_assets"),
       "@shared": path.resolve(__dirname, "shared"),
     },
   },
   build: {
-    // Viteにビルドの開始点を教えます
     rollupOptions: {
-      input: path.resolve(__dirname, "client/index.html"),
+      // ★★★ ここのパスを修正しました ★★★
+      input: path.resolve(__dirname, "client/src/index.html"),
     },
-    // 完成したファイルの出力先をプロジェクトのルート直下の`dist`フォルダに指定
     outDir: path.resolve(__dirname, "dist"),
   },
 });
